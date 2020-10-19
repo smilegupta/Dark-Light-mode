f96b6c7645821f5381f489fc4ae913cee9354eec
smilegupta <guptamiley3012@gmail.com>
2020-10-19 16:37:41 +0530

    basic toggle done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import './App.css';
+import Content from './components/Content';
+import styled, {ThemeProvider} from 'styled-components';
+import { UserDarkMode } from './styles/UserDarkMode'
+import Toggle from './components/Toggle'
+import {GlobalStyle, lightTheme, darkTheme} from './styles/GlobalStyle'
+
+const Container = styled.div `
+max-width: 50%;
+margin: 8rem auto 0;
+`;
 
 function App() {
+  const [theme, toggleTheme] = UserDarkMode()
+  const themeMode  = theme === 'light' ? lightTheme : darkTheme;
+  console.log(theme)
   return (
-    <div className="App">
-      
-    </div>
+    <ThemeProvider theme={themeMode}>
+    <Container>
+      <GlobalStyle />
+      <Toggle theme={theme} toggleTheme={toggleTheme}/>
+      <Content />
+    </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Content.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div>
+      <h1>Sapien eget mi proin sed libero</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Pharetra sit amet
+        aliquam id diam. Et ultrices neque ornare aenean euismod. Elit eget
+        gravida cum sociis natoque penatibus. Lorem ipsum dolor sit amet
+        consectetur adipiscing elit. Eu sem integer vitae justo. 
+      </p>
+      <button className="btn-primary"> Read More</button>
+    </div>
+  );
+};
+
+export default Content;
